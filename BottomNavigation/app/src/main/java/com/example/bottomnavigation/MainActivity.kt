ad63e4b5575a589cb4f636f616ccaab59237aa61
ka <insideka01@gmail.com>
2022-01-26 21:52:49 +0900

    01.26 bottomnavigation + fragment

--- BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.bottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val menuFragment by lazy { MenuFragment() }
+    private val cartFragment by lazy { CartFragment() }
+    private val my100Fragment by lazy { My100Fragment() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameLayout, fragment)
+            .commit()
     }
 }
