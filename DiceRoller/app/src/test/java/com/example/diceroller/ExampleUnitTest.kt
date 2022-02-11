ce6032822db54532bb9a29e4766e09985a69742b
ka <insideka01@gmail.com>
2022-02-11 17:07:48 +0900

    22.02.11 Write unit test

--- DiceRoller/app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ DiceRoller/app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,6 +1,6 @@
 package com.example.diceroller
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 /**
@@ -9,8 +9,11 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 3)
+    fun generate_number() {
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
     }
 }
