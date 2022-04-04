7f8225250a1fe88911ae9de97b61bd83127b221a
ka <insideka01@gmail.com>
2022-04-04 20:57:33 +0900

    04.04 Fragments and the Navigation Component - Using the Navigation Graph

--- WordsWithFragment/app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ WordsWithFragment/app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -18,8 +18,12 @@ package com.example.wordsapp
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.SurfaceControl
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -30,11 +34,22 @@ import com.example.wordsapp.databinding.ActivityMainBinding
  */
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
