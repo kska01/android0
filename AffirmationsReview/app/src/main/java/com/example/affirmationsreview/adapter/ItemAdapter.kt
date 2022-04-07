111a216aa0bbdf54af08214789a15ad17c80444c
ka <insideka01@gmail.com>
2022-04-07 21:22:39 +0900

    04.07 RecyclerView Review - Adding a RecyclerView to your app

---
+++ AffirmationsReview/app/src/main/java/com/example/affirmationsreview/adapter/ItemAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.affirmationsreview.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmationsreview.R
+import com.example.affirmationsreview.model.Affirmation
+
+class ItemAdapter(private val context: Context, val dataset: List<Affirmation>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+    override fun getItemCount() = dataset.size
+}
