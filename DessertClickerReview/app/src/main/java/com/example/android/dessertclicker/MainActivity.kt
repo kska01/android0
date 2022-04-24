d0714975c4f9e5ccf78bf636af25a95d3755a5d7
ka <insideka01@gmail.com>
2022-04-24 17:41:32 +0900

    04.24 Stages of the activity lifecycle - Explore configuration changes

--- DessertClickerReview/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ DessertClickerReview/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.dessertclicker
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -97,6 +98,12 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume Called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        Log.d(TAG, "onSaveInstanceState Called")
     }
 
     override fun onPause() {
