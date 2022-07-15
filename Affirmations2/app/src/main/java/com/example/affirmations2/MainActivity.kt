ff39c4db9a9289333dc99e46134e9571043790e2
ka <insideka01@gmail.com>
2022-07-15 16:50:15 +0900

    07.15 Use RecyclerView to display a scrollable list - complete

--- Affirmations2/app/src/main/java/com/example/affirmations2/MainActivity.kt
+++ Affirmations2/app/src/main/java/com/example/affirmations2/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.affirmations2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations2.adapter.ItemAdapter
 import com.example.affirmations2.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerview = findViewById<RecyclerView>(R.id.recycler_view)
+        // To tell the recyclerview to use the ItemAdapter class.
+        recyclerview.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the Recyclerview
+        recyclerview.setHasFixedSize(true)
     }
 }
