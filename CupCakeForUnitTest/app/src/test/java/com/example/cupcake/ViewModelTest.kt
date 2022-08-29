b7ed1c4fb7ce7c054497abd4bfdcfea33a8a5136
ka <insideka01@gmail.com>
2022-08-29 22:46:48 +0900

    08.29 Test ViewModels and LiveData - Write a ViewModel test

---
+++ CupCakeForUnitTest/app/src/test/java/com/example/cupcake/ViewModelTest.kt
@@ -0,0 +1,10 @@
+package com.example.cupcake
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+
+class ViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+}
