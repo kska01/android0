b7b682d1593ca559b2fdfe204df1c4cc680dc9d5
ka <insideka01@gmail.com>
2022-08-30 11:06:31 +0900

    08.30 Test ViewModels and LiveData - complete

--- CupCakeForUnitTest/app/src/test/java/com/example/cupcake/ViewModelTest.kt
+++ CupCakeForUnitTest/app/src/test/java/com/example/cupcake/ViewModelTest.kt
@@ -1,10 +1,34 @@
 package com.example.cupcake
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.cupcake.model.OrderViewModel
+import org.junit.Assert.assertEquals
 import org.junit.Rule
+import org.junit.Test
 
 class ViewModelTest {
 
+        // To specify that LiveData objects should not call the main thread we need to
+    // provide a specific test rule any time we are testing a LiveData object.
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+
+    @Test
+    fun quantity_twelve_cupcakes() {
+        val viewModel = OrderViewModel()
+        // To add observeForever for observing changes of values of LiveData object.
+        // observeForever method requires a lambda expression, but that can be left empty.
+        viewModel.quantity.observeForever {}
+        viewModel.setQuantity(12)
+        assertEquals(12, viewModel.quantity.value)
+    }
+
+    @Test
+    fun price_twelve_cupcakes() {
+        val viewModel = OrderViewModel()
+        viewModel.price.observeForever {}
+        viewModel.setQuantity(12)
+        assertEquals("$27.00", viewModel.price.value)
+    }
 }
