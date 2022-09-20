ca970b60b44016bf172af176f51ed94f5cb403b8
ka <insideka01@gmail.com>
2022-09-20 20:37:40 +0900

    09.20 Get data from the internet - Add Internet permission and Exception Handling

--- MarsPhotos/app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ MarsPhotos/app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -5,6 +5,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
+
 // Retrofit 은  웹 서비스의 기본 URI 와 converter factory 가 필요하다.
 // converter factory 중 ScalarsConverterFactory 는 Kotlin 의 String 과 원시 타입들을 지원한다.
 private val retrofit = Retrofit.Builder()
@@ -12,6 +13,7 @@ private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .build()
 
+// Create an interface that explain how Retrofit talks to our web sever
 interface MarsApiService {
     // Retrofit 에게 GET 요청임을 알려주기 위해 @GET annotation 을 사용한다. 그리고 endpoint 로
     // "photos" 를 명시한다.
@@ -19,6 +21,7 @@ interface MarsApiService {
     suspend fun getPhotos(): String
 }
 
+// Create a Retrofit service and expose the instance to the api service to the rest of the app.
 object MarsApi {
     val retrofitService : MarsApiService by lazy {
         retrofit.create(MarsApiService::class.java)
