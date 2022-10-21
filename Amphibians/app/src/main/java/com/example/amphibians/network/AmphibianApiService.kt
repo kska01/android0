153fb12f76c02a7b0485791e3b8c544f7bea38d6
ka <insideka01@gmail.com>
2022-10-21 15:09:46 +0900

    10.21 Amphibian app - complete

--- Amphibians/app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
+++ Amphibians/app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
@@ -19,6 +19,7 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
 
 // TODO: Create a property for the base URL provided in the codelab
 private const val BASE_URL = "https://developer.android.com/courses/pathways/android-basics-kotlin-unit-4-pathway-2/"
@@ -36,7 +37,8 @@ private val retrofit = Retrofit.Builder()
 
 interface AmphibianApiService {
     // TODO: Declare a suspended function to get the list of amphibians
-    suspend fun getList(): String
+    @GET("android-basics-kotlin-unit-4-pathway-2-project-api.json")
+    suspend fun getAmphibians(): List<Amphibian>
 }
 
 // TODO: Create an object that provides a lazy-initialized retrofit service
