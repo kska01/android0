0456df1ffb58e06264f4383a25f20db482dda1d0
ka <insideka01@gmail.com>
2023-02-20 23:39:04 +0900

    5.Define the DAO 까지 함

---
+++ BusSchedule/app/src/main/java/com/example/busschedule/database/schedule/Schedule.kt
@@ -0,0 +1,13 @@
+package com.example.busschedule.database.schedule
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity // Room이 database의 table로 사용할 수 있도록 알려주는 annotation
+data class Schedule(
+    @PrimaryKey val id: Int,
+    @NonNull @ColumnInfo(name = "stop_name") val stopName: String,
+    @NonNull @ColumnInfo(name = "arrival_time") val arrivalTime: Int
+)
