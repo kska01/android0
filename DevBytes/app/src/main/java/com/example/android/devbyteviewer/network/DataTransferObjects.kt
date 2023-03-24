f528a36c0fb3a24be754a8355efce272d381c6a3
ka <insideka01@gmail.com>
2023-03-24 22:09:22 +0900

    03.24 5.Task: Add on offline cache - step3: Add VideoDao까지 함

--- DevBytes/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ DevBytes/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.devbyteviewer.network
 
+import com.example.android.devbyteviewer.database.DatabaseVideo
 import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.squareup.moshi.JsonClass
 
@@ -52,7 +53,7 @@ data class NetworkVideo(
         val closedCaptions: String?)
 
 /**
- * Convert Network results to database objects
+ * Convert Network results to domain objects
  */
 fun NetworkVideoContainer.asDomainModel(): List<DevByteVideo> {
     return videos.map {
@@ -62,5 +63,20 @@ fun NetworkVideoContainer.asDomainModel(): List<DevByteVideo> {
                 url = it.url,
                 updated = it.updated,
                 thumbnail = it.thumbnail)
+    }
+}
+
+/**
+ * Convert Network results to database objects
+ */
+fun NetworkVideoContainer.asDatabaseModel(): List<DatabaseVideo> {
+    return videos.map {
+        DatabaseVideo(
+            title = it.title,
+            description = it.description,
+            url = it.url,
+            updated = it.updated,
+            thumbnail = it.thumbnail
+        )
     }
 }
